51030d5511a7a080de1b0a478192436725682fa2
Igor-JCI <7.jciab.7@gmail.com>
2022-08-17 19:07:15 +0300

    03-map

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import {ArrayType, Todolist} from "./Todolist";
 
 function App() {
     let tasks1: Array<ArrayType> = [
-        {id: 1, title: "CSS", isDone: true},
-        {id: 1, title: "JS", isDone: true},
-        {id: 1, title: "React", isDone: false}
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false}
     ]
     let tasks2: Array<ArrayType> = [
-        {id: 1, title: "Terminator", isDone: true},
-        {id: 1, title: "XXX", isDone: false},
-        {id: 1, title: "Gentleman of fortune ", isDone: true}
+        {id: 1, title: "Hello word", isDone: true},
+        /*{id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo ", isDone: false}*/
     ]
     return (
         <div className="App">
             <Todolist title="What to learn" tasks = {tasks1}/>
-            <Todolist title="Movies" tasks = {tasks2}/>
+            <Todolist title="Songs" tasks = {tasks2}/>
         </div>
     );
 }
