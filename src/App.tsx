5bd299298c42d9db339ebcdb5c786ad52e4f90f6
Igor-JCI <7.jciab.7@gmail.com>
2022-08-18 19:01:29 +0300

    05-useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,46 @@ import React, {useState} from 'react';
 import './App.css';
 import {ArrayType, Todolist} from "./Todolist";
 
+
+/*export const Counter = () => {
+    debugger
+    console.log("rendering")
+    let arr = useState(5)
+    let data = arr[0]
+    let setData = arr[1]
+
+    return (
+        <div onClick={() => {
+            setData(data - 1)
+        }}>{data}</div>
+    )
+}*/
+
 function App() {
-    let tasks: Array<ArrayType> = [
+    /*let initTasks: Array<ArrayType> = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "Redux", isDone: false}
     ]
+    let [tasks, setTasks] = useState(initTasks)*/
 
 
+    let [tasks, setTasks] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false}
+    ])
 
     const removeTask = (id: number) => {
-
-        tasks = tasks.filter(t => t.id !== id)
         debugger
-          /*let resultTask = tasks.filter((t) => {
-              return t.id !== id
-          })*/
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks)
+
+        /*let resultTask = tasks.filter((t) => {
+            return t.id !== id
+        })*/
 
         /*  let resultTask = tasks.filter((t) => {
               if (t.id !== id) {
