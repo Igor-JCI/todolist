25a390b22a3e22e4953c0ea53996b653b074570f
Igor-JCI <7.jciab.7@gmail.com>
2023-01-28 12:10:43 +0500

    07-code refactoring 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
-import {ArrayType, Todolist} from "./Todolist";
+import {Todolist} from "./Todolist";
 import {v1} from "uuid";
-
-
 /*export const Counter = () => {
     debugger
     console.log("rendering")
@@ -17,9 +15,7 @@ import {v1} from "uuid";
         }}>{data}</div>
     )
 }*/
-
 export type FilterValuesType = "all" | "active" | "completed"
-
 
 function App() {
     /*let initTasks: Array<ArrayType> = [
@@ -29,14 +25,12 @@ function App() {
         {id: 4, title: "Redux", isDone: false}
     ]
     let [tasks, setTasks] = useState(initTasks)*/
-
     let [tasks, setTasks] = useState([
         {id: v1(), title: "HTML&CSS", isDone: true},
         {id: v1(), title: "JS", isDone: true},
         {id: v1(), title: "ReactJS", isDone: false},
         {id: v1(), title: "Redux", isDone: false}
     ])
-
     let [filter, setFilter] = useState<FilterValuesType>("all")
 
     const changeStatus = (taskId: string, isDone: boolean) => {
@@ -44,29 +38,21 @@ function App() {
         if (task) {
             task.isDone = isDone
         }
-
         setTasks([...tasks])
-
     }
-
     const addTask = (title: string) => {
         let newTask = {
             id: v1(),
             title: title,
             isDone: false
         }
-
         let newTasks = [newTask, ...tasks]
         setTasks(newTasks)
     }
-
-
     const changeFilter = (value: FilterValuesType) => {
         setFilter(value)
     }
-
     const removeTask = (id: string) => {
-        debugger
         let filteredTasks = tasks.filter(t => t.id !== id)
         setTasks(filteredTasks)
     }
@@ -98,5 +84,4 @@ function App() {
         </div>
     );
 }
-
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterValuesType} from "./App";
-
-
-
 export type ArrayType = {
     id: string
     title: string
