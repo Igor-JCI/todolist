6b12ba234b3ba37fd2ce416d32bcf34de2259a3e
Igor-JCI <7.jciab.7@gmail.com>
2023-06-03 22:07:48 +0300

    16-14-02-thunk refactoring 9.0

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./state/store";
 import {AppWithRedux} from "./AppWithRedux";
-
 ReactDOM.render(
     <Provider store={store}>
         <AppWithRedux/>
