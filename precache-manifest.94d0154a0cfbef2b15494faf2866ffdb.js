03841c29d53a56b3bd3c6c9a640ad4c73e1b4fd9
Igor-JCI <7.jciab.7@gmail.com>
2024-01-28 16:39:10 +0300

    Updates

---
+++ precache-manifest.94d0154a0cfbef2b15494faf2866ffdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d5effc7ee150294a44eb725e021f9a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "32b401093bc0644e164d",
+    "url": "/todolist/static/css/main.13752046.chunk.css"
+  },
+  {
+    "revision": "8a0536c63ebac74e4c57",
+    "url": "/todolist/static/js/2.766e7048.chunk.js"
+  },
+  {
+    "revision": "f1351e6162f20f6686eea6885ea66295",
+    "url": "/todolist/static/js/2.766e7048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b401093bc0644e164d",
+    "url": "/todolist/static/js/main.41dfc884.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todolist/static/js/main.41dfc884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ebc9bad2a2c9c79d0c",
+    "url": "/todolist/static/js/runtime-main.39f6e27a.js"
+  }
+]);
