d83b280dfd67ec02c05f4a65c082f8f553a11465
Igor-JCI <7.jciab.7@gmail.com>
2024-01-31 22:15:08 +0300

    22-20 - refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -67,7 +67,7 @@ function App({demo = false}: PropsType) {
             </AppBar>
             <Container fixed>
                 <Routes>
-                    <Route path={"/"} element={<TodolistsList demo={demo}/>}/>
+                    <Route path={"/todolist"} element={<TodolistsList demo={demo}/>}/>
                     <Route path={"/login"} element={<Login/>}/>
                 </Routes>
             </Container>
